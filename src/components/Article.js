11f9efde79a77ce29da5710510ccdda862928e59
Mark Kelsall <markkelsall1995@gmail.com>
2024-02-02 15:55:50 +0300

    final

---
+++ src/components/Article.js
@@ -0,0 +1,31 @@
+// Article.js
+import React from 'react';
+
+const Article = ({ title, date, preview, minutes }) => {
+  const defaultDate = "January 1, 1970";
+
+  // Function to calculate 'Minutes to Read'
+  const calculateMinutesToRead = (minutes) => {
+    const coffeeCupEmoji = "☕️";
+    const bentoBoxEmoji = "🍱";
+    const coffeeCupPerFiveMinutes = Math.ceil(minutes / 5);
+    const bentoBoxPerTenMinutes = Math.ceil(minutes / 10);
+
+    if (minutes < 30) {
+      return coffeeCupEmoji.repeat(coffeeCupPerFiveMinutes) + ` ${minutes} min read`;
+    } else {
+      return bentoBoxEmoji.repeat(bentoBoxPerTenMinutes) + ` ${minutes} min read`;
+    }
+  };
+
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>{date || defaultDate}</small>
+      <p>{preview}</p>
+      <p>{calculateMinutesToRead(minutes)}</p>
+    </article>
+  );
+};
+
+export default Article;
---
+++ src/components/ArticleList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Article from './Article';  
+const ArticleList = ({ posts }) => {
+  return (
+    <main>
+      {posts.map((post, index) => (
+        <Article
+          key={index}
+          title={post.title}
+          date={post.date}
+          preview={post.preview}
+          minutes={post.minutes}
+        />
+      ))}
+    </main>
+  );
+};
+
+export default ArticleList;
